42b7382e5142b829e9ec70c33e532ed69f320f51
Jay Ford <jay.ford45@gmail.com>
2022-03-24 12:49:33 -0400

    api pull now pulls with form inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-let maxCal=1000
-let minCal = 0
-let maxProt = 100
-let minProt = 0
-let maxCarbs = 90
-let minCarbs = 0
-let maxFat = 100
-let minFat = 0
+let maxCalEl = $("#maxCal");
+let minCalEL = $("#minCal");
+let maxProtEl = $("#maxProt");
+let minProtEl = $("#minProt");
+let maxCarbsEl = $("#maxCarb");
+let minCarbsEl = $("#minCarb");
+let minFatEl = $("#minFat");
+let maxFatEl = $("#maxFat");
+
 let recipeListEl = document.querySelector(".recipes")
 let offset = 0;
 
@@ -15,7 +16,15 @@ let offset = 0;
 
 
 function getRecipeInfo() {
-
+    let maxCal = maxCalEl.val()
+    let minCal = minCalEL.val()
+    let maxProt = maxProtEl.val()
+    let minProt = minProtEl.val()
+    let maxCarbs = maxCarbsEl.val()
+    let minCarbs = minCarbsEl.val()
+    let maxFat = maxFatEl.val()
+    let minFat = minFatEl.val()
+    console.log(maxCal)
     let apiUrl = "https://api.spoonacular.com/recipes/complexSearch?maxCalories=" + maxCal + "&minCalories=" + minCal + "&maxProtein=" + maxProt + "&minProtein=" + minProt + "&maxCarbs=" + maxCarbs + "&minCarbs=" + minCarbs + "&maxFat=" + maxFat + "&minFat=" + minFat + "&sort=popularity&offset=" + offset + "&number=10&apiKey=12e90f7110fa407caf3c0a919ae2be54";
 
     let cardDivEl = document.createElement("div")
@@ -55,7 +64,9 @@ function getRecipeInfo() {
     recipeListEl.append(cardDivEl)
 }
 
-getRecipeInfo();
+$("#gen-btn").on("click", function() {
+    getRecipeInfo();
+})
 
 $(".recipe-cards").sortable({
     connectWith: $(".calendar"),
